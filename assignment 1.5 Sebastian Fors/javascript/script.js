2c97dfad6f09fd1f901b53ce5bda8e714c40a48f
sebastianforce <sebastian.fors@gritacademy.se>
2024-10-22 19:36:18 +0200

    uppgift-1.5

---
+++ assignment 1.5 Sebastian Fors/javascript/script.js
@@ -0,0 +1,12 @@
+function run() {
+    let vip = document.getElementById("vipcheckbox").checked;
+    let age = document.getElementById("field1").valueAsNumber;
+    if (vip) alert("Welcome in my best VIP customer :)");
+    else {
+        if (age >= 35) alert("Access denied TOO OLD!");
+        else {
+            if (age >= 18) alert("Access granted");
+            else alert("Access denied");
+        }
+    }
+}
