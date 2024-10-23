597351a7e2c770d0aaca3c5bd95d4f446b452ea6
Alrik He <alrik_he@hotmail.com>
2024-10-23 11:17:36 +0200

    fixed

--- assignment 1.5 Sebastian Fors/javascript/script.js
+++ assignment 1.5 Sebastian Fors/javascript/script.js
@@ -1,12 +1,16 @@
 function run() {
     let vip = document.getElementById("vipcheckbox").checked;
     let age = document.getElementById("field1").valueAsNumber;
-    if (vip) alert("Welcome in my best VIP customer :)");
+    if (vip) 
+        alert("Welcome in my best VIP customer :)");
     else {
-        if (age >= 35) alert("Access denied TOO OLD!");
+        if (age >= 35) 
+            alert("Access denied TOO OLD!");
         else {
-            if (age >= 18) alert("Access granted");
-            else alert("Access denied");
+            if (age >= 18) 
+                alert("Access granted");
+            else 
+                alert("Access denied");
         }
     }
 }
